f31d00360b9945dc244c5bc693dc47e31385b022
Alexander <alexandergaruba96@gmail.com>
2019-05-21 12:57:02 +0100

    Question 2

--- javascript/question2.js
+++ javascript/question2.js
@@ -1,6 +1,31 @@
 // Complete the beautifulDays function below.
+let i = 20;
+let j = 23;
+let k = 6;
+
 function beautifulDays(i, j, k) {
-  return null;
+	var days = [];
+	while (i <= j) {
+		days.push(i++);
+	}
+
+	const checkBeautifulDay = days => {
+		return days.map(day => {
+			let reversedDate = parseFloat(
+				day
+					.toString()
+					.split('')
+					.reverse()
+					.join('')
+			);
+
+			return Number.isInteger((day - reversedDate) / k);
+		});
+	};
+
+	console.log(checkBeautifulDay(days));
 }
 
-module.exports = beautifulDays
+// module.exports = beautifulDays;
+
+beautifulDays(i, j, k);
